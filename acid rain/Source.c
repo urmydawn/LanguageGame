68dcdf7992592fb4ae6137334a3f19d0c1d53df5
Kimdawn <tont0807@naver.com>
2024-10-04 21:44:01 +0900

    (2024.10.4) acid rain (60%)

--- acid rain/Source.c
+++ acid rain/Source.c
@@ -5,7 +5,7 @@
 #define WIDTH 20
 #define HEIGHT 10
 
-typedef struct
+typedef struct // 비 구조체
 {
 	int x;
 	int y;
@@ -13,7 +13,7 @@ typedef struct
 	int active;
 }Rain;
 
-typedef struct
+typedef struct // 플레이어 구조체
 {
 	int x;
 }Player;
@@ -21,7 +21,7 @@ typedef struct
 Rain rain[WIDTH];
 Player player;
 
-void gotoxy(int x, int y)
+void gotoxy(int x, int y) // 커서 이동 함수
 {
 	COORD pos = { 2 * x, y };
 	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
@@ -32,107 +32,121 @@ int PressKey(int key) // 키가 눌려졌는지
 	return ((GetAsyncKeyState(key) & 0x8000) != 0);
 }
 
-void PrintMap()
+void Reset() // 초기화 함수
 {
-	system("cls");
+	int i;
+	for (i = 0; i < WIDTH; i++)
+	{
+		rain[i].active = 0;
+		player.x = WIDTH / 2;
+	}
+}
+
+void PrintMap() // 맵 출력 함수
+{
+	system("cls"); // 화면 클리어
 
 	int i;
 	for (i = 0; i < WIDTH; i++)
 	{
-		if (rain[i].active)
+		if (rain[i].active) // 비가 활성화 됐다면
 		{
 			gotoxy(rain[i].x, HEIGHT - rain[i].y);
-			printf("l");
+			printf("l"); // 비
 		}
 	}
 	gotoxy(player.x, HEIGHT);
-	printf("♥");
+	printf("♥"); // 플레이어
 
 	gotoxy(0, HEIGHT + 1);
 	for (i = 0; i < WIDTH; i++)
-		printf("■");
+		printf("■"); // 바닥
 }
 
-void PlayerMove()
+void MovePlayer() // 플레이어 움직이는 함수
 {
-	if (PressKey(VK_LEFT));
-	player.x -= 1;
-	if (PressKey(VK_RIGHT));
-	player.x += 1;
+	if (PressKey(VK_LEFT)) // 왼쪽 방향키가 눌러지면
+	player.x -= 1; // 왼쪽으로 한칸
+	if (PressKey(VK_RIGHT)) // 오른쪽 방향키가 눌러지면
+	player.x += 1; // 오른쪽으로 한칸
 
-	if (player.x < 0);
-	player.x = 0;
-	if (player.x > WIDTH - 1);
-	player.x = WIDTH - 1;
+	if (player.x < 0) // 플레이어x가 0보다 작으면
+		player.x = 0; // 0으로 설정
+	if (player.x > WIDTH - 1) // 플레이어x가 WIDTH(20) - 1 = 19면
+		player.x = WIDTH - 1; // 19로 설정
 }
 
-void CreateRain()
+void CreateRain() // 비 생성 함수
 {
 	int i;
 
 	for (i = 0; i < WIDTH; i++)
 	{
-		if (!rain[i].active)
+		if (!rain[i].active) // 비가 활성화 되지 않으면 (NOT)
 		{
 			rain[i].x = rand() % WIDTH;
 			rain[i].y = HEIGHT - 1;
 
-			rain[i].active = 1;
+			rain[i].active = 1; // 비를 활성화
 
 			return;
 		}
 	}
 }
 
-void MoveRain()
+void MoveRain() // 비 움직이는 함수
 {
 	int i;
 	for (i = 0; i < WIDTH; i++)
 	{
-		if (rain[i].active);
+		if (rain[i].active) // 비가 활성화 되면
 		{
-			rain[i].y--;
+			rain[i].y--; // 비의 y 감소
 		}
 	}
 }
 
-void DeleteRain()
+void DeleteRain() // 비 제거 함수
 {
 	int i;
 	for (i = 0; i < WIDTH; i++)
 	{
-		if (rain[i].active && rain[i].y < 0)
+		if (rain[i].active && rain[i].y < 0) // 비가 활성화 되고 비y가 0보다 작으면
 		{
-			rain[i].active = 0;
+			rain[i].active = 0; // 비 비활성화
 		}
 	}
 }
 
-void PlayerWetRain()
+int PlayerTouchRain() // 플레이어가 비에 닿으면
 {
 	int i;
 	for (i = 0; i < WIDTH; i++)
 	{
-		if (rain[i].active && (rain[i].y == 0 && rain[i].x == player.x));
+		if (rain[i].active && (rain[i].y == 0 && rain[i].x == player.x)) // 비가 활성화 되고, 비y가 0이랑 같고, 비x가 플레이어x랑 같으면
 		{
-			printf("\n----- 탈락 -----\n");
+			printf("\n           ----- 탈락 -----\n");
 			return 1;
 		}
 	}
-	return 0;
+	return 0; // 탈락
 }
 
-
-/*void main()
+void main()
 {
-	system("cls");
+	char key; // 입력 키 변수
 
-	char key;
+	Reset(); // 초기화 함수
 
-
+	do // 반복문
 	{
-	PrintMap();
-	PlayerMove();
+		srand((int)malloc(NULL));
 
-	}
-}*/
+		PrintMap(); // 맵 출력 함수
+		MovePlayer(); // 플레이어 움직이는 함수
+		CreateRain(); // 비 생성 함수
+		MoveRain(); // 비 움직이는 함수
+		DeleteRain(); // 비 제거 함수
+		Sleep(60); // 속도 제한
+	} while (!(PlayerTouchRain())); // 비가 닿이지 않으면 반복 (NOT)
+}
